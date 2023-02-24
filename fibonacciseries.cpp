969fe9ed0aaa7d4a09e4224f2ecdcaee0230e328
GUL KHAN <105405655+gulkhan799@users.noreply.github.com>
2023-02-24 19:21:48 +0530

    Update fibonacciseries.cpp

--- fibonacciseries.cpp
+++ fibonacciseries.cpp
@@ -5,6 +5,8 @@ int main()
     int n;
     cout<<"Enter how many tern you want after 0 and 1"<<endl;
     cin>>n;
+    // nextnumber is made up of  sum of teo perivios numbers
+    // 0 1 1 2 3 5 8 13 21 ....
     int a=0;
     int b=1;
      cout<<a<<endl<<b<<endl;
